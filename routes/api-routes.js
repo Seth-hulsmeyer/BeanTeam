14ba70b8e275b041e0b88fe31ef7d06e0261515e
Sam Fan <samisfan@msn.com>
2021-03-03 12:05:46 -0700

    fixed file path in api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-const passport = require("../client/src/config/passport");
+const passport = require("../config/passport");
 // const isAuthenticated = require("../client/src/config/middleware/isAuthenticated");
 // const passwordStrength = require("check-password-strength");
 
