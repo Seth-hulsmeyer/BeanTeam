bc2b881c8333be4f9e5d23133901611b8ab04000
Seth-hulsmeyer <seth.hulsmeyer@hotmail.com>
2021-03-02 20:55:48 -0700

    test branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/login" component={Login} />
+    </Router>
   );
 }
-
 
 export default App;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const session = require("express-session");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const path = require("path");
 const passport = require("passport");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Define middleware here
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 }
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
@@ -20,14 +20,14 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 // Define API routes here
-app.use(routes)
+// app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bean_feeder");
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, () => {
