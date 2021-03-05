d37b08d41e2bef8f0d4613bf861e85eced2f3b4b
Sam Fan <samisfan@msn.com>
2021-03-04 18:04:02 -0700

    added logos and beginning functions for fetching video array for Main page

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
+import VideoButton from "../components/VideoButton";
 
 function Main() {
   // UseEffect to call the backend. Get user info based off of ID
-
-  return <h1>THIS IS THE MAIN PAGE!</h1>;
+  return (
+    <>
+      <Container>
+        <h1>THIS IS THE MAIN PAGE!</h1>
+        <VideoButton />
+      </Container>
+    </>
+  );
 }
 
 export default Main;
